8991d04148f74ffae09dea8c29a886df5e8fb3e3
decmca <153081807+decmca@users.noreply.github.com>
2025-11-05 13:03:53 +0000

    initial test

---
+++ .history/src/portfolio_20251104154549.js
@@ -0,0 +1,71 @@
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  homepage: '',
+  title: '',
+}
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: 'Declan McAlinden',
+  role: 'Data Scientist / Artificial Intelligence Researcher',
+  picture: 'https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png',
+
+  description:
+    'Add description.',
+  resume: 'https://example.com',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/dmuk/',
+    github: 'https://github.com/decmca',
+  },
+}
+
+const projects = [
+  // projects can be added an removed
+  // if there are no projects, Projects section won't show up
+  {
+    name: 'Early prediction of Alzheimer\'s using Network Spreading Models.',
+    description:
+      'Add Description',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+    image: 'cleanfolio.png',
+  },
+  {
+    name: 'Project 2',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+    image: 'https://github.githubassets.com/assets/GitHub-Logo-ee398b662d42.png',
+  },
+  {
+    name: 'Project 3',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+]
+
+const skills = [
+  // skills can be added or removed
+  // if there are no skills, Skills section won't show up
+  'Python',
+  'Machine Learning',
+  'Artificial Intelligence',
+  'Git',
+  'CI/CD',
+  '3LC',
+  'Alteryx',
+  'Streamlit'
+]
+
+const contact = {
+  // email is optional - if left empty Contact section won't show up
+  email: '',
+}
+
+export { header, about, projects, skills, contact }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/rjshkhr/cleanfolio'
+      className='link footer__link'
+    >
+      Portfolio is adapted from the Cleanfolio template created by Raj Shekhar
+    </a>
+  </footer>
+)
+
+export default Footer
